df216f0d8e92bd07eab691e4cbef0938a59e44c8
René Daniel <dgalicia@interesse.com.mx>
2017-05-21 19:18:57 -0500

    Se agrega componente de carga

---
+++ src/components/loading.js
@@ -0,0 +1,30 @@
+import React, { Component} from 'react';
+import {Row, Col} from 'react-materialize';
+
+const Loading = ({error}) => {
+  if (error) {
+    return(
+      <Row className="detalle loading center-align">
+        <Col s={12}>La serie o temporada que buscas no existe</Col>
+      </Row>
+    );
+  }
+  return (
+    <Row className="detalle loading center-align">
+      <Col s={12}>Cargando datos</Col>
+      <div className="preloader-wrapper big active">
+        <div className="spinner-layer spinner-blue-only">
+          <div className="circle-clipper left">
+            <div className="circle"></div>
+          </div><div className="gap-patch">
+            <div className="circle"></div>
+          </div><div className="circle-clipper right">
+            <div className="circle"></div>
+          </div>
+        </div>
+      </div>
+    </Row>
+  );
+};
+
+export default Loading;
